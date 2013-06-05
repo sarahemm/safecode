d6025a1f101a2c4e8f561a28dfbf7106420ae098
Sen <github@sen.cx>
2013-06-05 18:40:18 -0400

    Added reconnecting to the SafeCode box when the connection is lost. Next step is adding reporting of this status.

--- daemon/safecode-daemon.rb
+++ daemon/safecode-daemon.rb
@@ -14,10 +14,19 @@ EM.run {
   ws = Faye::WebSocket::Client.new(webservice_url)
   #status = {:box_connected => true, :session_state => :not_in_session, :session_start => Time.now.to_i, :session_length => 2*60}
 
+  # block to check for new input and send it to the web service if it's complete
   EM::PeriodicTimer.new(1) do
-    box.each_char do |data|
-      buf += data
+    next if !box or !ws
+    begin
+      box.each_char do |data|
+        buf += data
+      end
+    rescue Errno::ENXIO
+      puts "lost connection to SafeCode box"
+      box = nil
+      next
     end
+    puts "Checking box"
     if(buf[-1] == '#') then
       cmd = Hash.new
       if(buf == "*#") then
@@ -28,6 +37,7 @@ EM.run {
         code = buf[0..-3]
         cmd[:event] = :check_out
         cmd[:code] = code
+        p cmd
         ws.send cmd.to_json
         puts "sending check-out request"
       else
@@ -40,6 +50,20 @@ EM.run {
         puts "sending check-in request for #{length} minute session"
       end
       buf = ""
+    end
+  end
+  
+  # block to check for lost connections and attempt reconnection
+  EM::PeriodicTimer.new(5) do
+    if(!box) then
+      puts "no connection to SafeCode box, reconnecting..."
+      begin
+        box = SerialPort.new(serial_port, 9600, 8, 1, SerialPort::NONE)
+        box.read_timeout = -1 # nonblock
+        puts "reconnected to SafeCode box"
+      rescue Errno::ENOENT
+        puts "box not plugged in"
+      end
     end
   end
   
